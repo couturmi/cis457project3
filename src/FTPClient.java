edf5dc8bc92c25a6eca7b05b931b72aaeb53e81b
Mitchell John Couturier <couturmi@mail.gvsu.edu>
2017-11-29 15:34:25 -0500

    update FTPClient to connect via static method

--- src/FTPClient.java
+++ src/FTPClient.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.net.*;
 import java.io.*;
-import java.util.*;
 import java.lang.*;
 
 
@@ -10,49 +9,35 @@ import java.lang.*;
  */
 public class FTPClient {
     private static JFrame frame;
-    private static Socket ControlSocket;
+    private static GameGUI panel;
 
-    public static void main(String argv[]) throws Exception {
+    private static Socket ControlSocket;
+    private static DataOutputStream toServer;
+    private static DataInputStream fromServer;
+
+    public static void main(String argv[]) {
         // Start GUI
         startGUI();
+    }
 
-        String input;
-        System.out.println("Type connect <ip> <port>");
-
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("FTPClient >> ");
-        input = inFromUser.readLine();
-        StringTokenizer tokens = new StringTokenizer(input);
-
-        if (input.startsWith("connect")) {
-            tokens.nextToken(); // connect
-            String serverName = tokens.nextToken(); // serverIP
-            int port = Integer.parseInt(tokens.nextToken()); // port
-            System.out.println("Connecting to " + serverName + ":" + port);
-            try {
-                ControlSocket = new Socket(serverName, port);
-            } catch (IOException ioEx) {
-                System.out.println("Unable to connect to " + serverName + ":" + port);
-                System.exit(1);
-            }
-            while (true) {
-                DataOutputStream toServer = new DataOutputStream(ControlSocket.getOutputStream());
-                DataInputStream fromServer = new DataInputStream(new BufferedInputStream(ControlSocket.getInputStream()));
-
-            }
-
-        } else {
-            System.out.println("You must connect to a server");
-            System.exit(1);
+    public static void connectToServer(String serverIP) throws Exception {
+        int port = 5568;
+        System.out.println("Connecting to " + serverIP + ":" + port);
+        try {
+            ControlSocket = new Socket(serverIP, port);
+        } catch (IOException ioEx) {
+            System.out.println("Unable to connect to " + serverIP + ":" + port);
+            return;
         }
-
+        toServer = new DataOutputStream(ControlSocket.getOutputStream());
+        fromServer = new DataInputStream(new BufferedInputStream(ControlSocket.getInputStream()));
     }
 
     private static void startGUI() {
         frame = new JFrame("Tic-Tac-Toe");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        GameGUI panel = new GameGUI(Player.USER);
+        panel = new GameGUI(Player.USER);
         frame.getContentPane().add(panel);
 
         frame.pack();
